9ef32d4b426d8637d8b11589e15e366639f94b1c
Nathan Panchal <nathanpanchal@gmail.com>
2015-06-02 15:24:21 -0700

    np - add abaility to handle multiple consenants in a row

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,10 +1,9 @@
 def translate(word)
   vowels = ['a', 'e', 'i', 'o', 'u']
   letters = word.split(//)
-  if vowels.include?(letters[0])
-    return letters.push("ay").join
-  else
-    letters.push(letters[0], 'ay').shift
-    return letters.join
+  until vowels.include?(letters[0])
+    letters.push(letters[0]).shift
   end
+  letters.push('ay').join
 end
+
