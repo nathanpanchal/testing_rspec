985ee433c1c7f106aff159223e5065bac03a08a5
Nathan Panchal <nathanpanchal@gmail.com>
2015-06-01 19:33:21 -0700

    np - add two new functions to simon says

--- simon_says.rb
+++ simon_says.rb
@@ -2,4 +2,15 @@ def echo(string)
   return string
 end
 
-echo('hello')
+def shout(string)
+  return string.upcase
+end
+
+def repeat(string, num = 'not specified')
+  num = 2 if num == 'not specified'
+  array = []
+  num.times do
+    array.push(string)
+  end
+  array.join(' ')
+end
