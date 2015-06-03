265437f8c742b0cb6a1ec05a2ed22e6a2fd17e1e
Nathan Panchal <nathanpanchal@gmail.com>
2015-06-02 21:51:35 -0700

    np - complete functionality for qu string

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,14 +1,12 @@
 def translate(word)
   vowels = ['a', 'e', 'i', 'o', 'u']
   letters = word.split(//)
-  
-  if letters[0]+letters[1] == 'qu'
-    letters.push(letters[0], letters[1]).shift(2)
-  end
-
   until vowels.include?(letters[0])
-    letters.push(letters[0]).shift
+    if letters[0]+letters[1] == 'qu'
+      letters.push(letters[0], letters[1]).shift(2)
+    else
+      letters.push(letters[0]).shift
+    end
   end
-
   letters.push('ay').join
 end
