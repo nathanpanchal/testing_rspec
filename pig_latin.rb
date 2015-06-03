a725525c9f617e6ed5d9e6a184c2867e5976d6ff
Nathan Panchal <nathanpanchal@gmail.com>
2015-06-02 21:44:34 -0700

    np - add recongition for qu string

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,9 +1,14 @@
 def translate(word)
   vowels = ['a', 'e', 'i', 'o', 'u']
   letters = word.split(//)
+  
+  if letters[0]+letters[1] == 'qu'
+    letters.push(letters[0], letters[1]).shift(2)
+  end
+
   until vowels.include?(letters[0])
     letters.push(letters[0]).shift
   end
+
   letters.push('ay').join
 end
-
