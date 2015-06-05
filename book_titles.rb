fa4b9b5e5c47abd3f9543e3c0d953bb393876e15
Nathan Panchal <nathanpanchal@gmail.com>
2015-06-05 13:59:28 -0700

    np - added bookean for first word check

--- book_titles.rb
+++ book_titles.rb
@@ -1,9 +1,10 @@
 class Book
   def cap(string)
+    exceptions = ['a', 'an', 'and', 'of', 'the']
     words = string.split(' ')
     words.each do |word|
-      if exceptions.include?(word)
-        
+      if exceptions.include?(word) && word != words[0]
+
     end
   end
 end
