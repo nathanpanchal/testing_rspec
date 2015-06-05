05fb6d6f0f6fe000d39377bce40b0f5ff6ff7ca6
Nathan Panchal <nathanpanchal@gmail.com>
2015-06-05 14:16:30 -0700

    np - attempt to add setter mentod

--- book_titles.rb
+++ book_titles.rb
@@ -1,10 +1,21 @@
 class Book
-  def cap(string)
-    exceptions = ['a', 'an', 'and', 'of', 'the']
+  def initialize(title)
+    @title = title
+  end
+
+  def title(title)
+    exceptions = ['a', 'an', 'and', 'in', 'of', 'the']
     words = string.split(' ')
     words.each do |word|
       if exceptions.include?(word) && word != words[0]
-
+      else
+        word.capitalize!
+      end
     end
+    return words.join(' ')
+  end
+
+  def title
+    @title
   end
 end
