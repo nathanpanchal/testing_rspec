323f72a7a8de6c96f409695cbe87cc851282e8ce
Nathan Panchal <nathanpanchal@gmail.com>
2015-06-07 09:40:58 -0700

    np - add split method for splitting seconds into hours, mins, and secs

--- timer.rb
+++ timer.rb
@@ -8,5 +8,23 @@ class Timer
     @seconds
   end
 
+  def split(input_seconds)
+    time = [3600, 60, 1]
+    time_remaining = input_seconds
+    time.map do |t|
+      n = time_remaining / t
+      time_remaining = time_remaining % t
+      # just in case someone inputs fractions of a second
+      n.to_i
+    end
+  end
+
+  def format(num)
+    if num < 10
+      return "0#{num}"
+    else
+      return num.to_s
+    end
+  end
 
 end
